0cfe7f8c96abb8a75d4f5d6c0bb467456cc614a7
ChibchaDev <juanbernalmintic@gmail.com>
2026-07-10 10:15:21 -0500

    feat: implement job management API with CORS support and pagination

---
+++ api/middlewares/cors.js
@@ -0,0 +1,20 @@
+import cors from 'cors'
+
+
+const ACCEPTED_ORIGINS = [
+    'http://localhost:5173',
+    'http://localhost:1234'
+]
+
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
+    return cors({
+        origin: (origin, callback) => {
+            if (ACCEPTED_ORIGINS.includes(origin) || !origin){
+                return callback(null, true)
+            }
+
+            return callback(new Error('Origen no permitido'))
+        }
+    })
+}
+
